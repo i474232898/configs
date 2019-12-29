068d0ff5ca709c945628a84d99e9876a7e28302e
i474232898 <i474232898@gmail.com>
2019-12-29 13:27:16 +0200

    add vscode; add init.sh

--- .zshrc
+++ .zshrc
@@ -1,8 +1,12 @@
+export ZSH="/Users/ivanromaniv/.oh-my-zsh"
+
 ZSH_THEME="agnoster"
 
 plugins=(
   git
 )
+
+source $ZSH/oh-my-zsh.sh
 
 alias g="git"
 alias n="node"
@@ -12,3 +16,17 @@ alias dcu="docker-compose up"
 alias doc="docker-compose"
 alias docps="docker ps --format 'table {{.Names}}\t{{.Image}}'"
 alias config='/usr/bin/git --git-dir=/Users/ivanromaniv/.cfg/ --work-tree=/Users/ivanromaniv'
+
+# Create dir and cd into it
+mkcd () {
+  mkdir "$1"
+  cd "$1"
+}
+
+# init Z
+. ~/Documents/local_apps/z.sh
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
---
+++ Documents/init.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh install.sh
+
+# Fonts for zsh
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+# Git aliases
+git config --global alias.lg "log --color --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --global alias.ch "checkout"
+git config --global alias.s "status -s"
+git config --global alias.tree "log --graph --oneline --all"
+
+# Z
+mkdir ~/Documents/local_apps
+git clone https://github.com/rupa/z.git ~/Documents/local_apps
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+
